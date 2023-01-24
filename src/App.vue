e88067bed3a55281cbc07094f784cabbff8cfa9e
kifuan <kifuan@foxmail.com>
2023-01-24 15:10:49 +0800

    feat: responsive

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import dayjs from 'dayjs'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const until = '2023/02/07'
 const untilDayjs = dayjs(until)
 const remaining = ref('')
+const clock = ref<HTMLDivElement>()
+const clockTop = ref('0px')
 
 function update() {
   const duration = dayjs.duration(untilDayjs.diff(dayjs()))
   remaining.value = duration.format('DD[d] HH[h] mm[m] ss[s]')
+  const top = document.body.clientHeight / 2 - clock.value!.clientHeight / 2
+  clockTop.value = `${top}px`
   requestAnimationFrame(update)
 }
 
-update()
+onMounted(() => {
+  update()
+})
 </script>
 
 <template>
-  <div class="clock">
+  <div class="clock" :style="{ marginTop: clockTop }" ref="clock">
     <p class="text">Time Remaining Until {{ until }}</p>
     <p class="time">{{ remaining }}</p>
   </div>
