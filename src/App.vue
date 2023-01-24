7348cee392ca3005ed99b4d9cc8e96012d706489
kifuan <kifuan@foxmail.com>
2023-01-24 15:22:32 +0800

    refactor: make the lower text smaller

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,8 @@ onMounted(() => {
 
 <template>
   <div class="clock" :style="{ marginTop: clockTop }" ref="clock">
-    <p class="text">Time Remaining</p>
+    <p class="remaining">Time Remaining</p>
     <p class="time">{{ remaining }}</p>
-    <p class="text">Until {{ until }}</p>
+    <p class="until">Until {{ until }}</p>
   </div>
 </template>
