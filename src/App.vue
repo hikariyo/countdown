3faa825bcc62464562a68bf7ccdca87cb97e4fed
kifuan <kifuan@foxmail.com>
2023-01-24 14:52:40 +0800

    feat: countdown

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <script setup lang="ts">
-import { useNow, useDateFormat } from '@vueuse/core'
+import dayjs from 'dayjs'
+import { ref } from 'vue'
 
-const now = useNow()
-const date = useDateFormat(now, 'YYYY-MM-DD')
-const time = useDateFormat(now, 'HH:mm:ss')
+const until = '2023/02/07'
+const untilDayjs = dayjs(until)
+const remaining = ref('')
+
+function update() {
+  const duration = dayjs.duration(untilDayjs.diff(dayjs()))
+  remaining.value = duration.format('DD[d] HH[h] mm[m] ss[s]')
+  requestAnimationFrame(update)
+}
+
+update()
 </script>
 
 <template>
   <div class="clock">
-    <p class="date">{{ date }}</p>
-    <p class="time">{{ time }}</p>
-    <p class="text">THIS IS TEXT</p>
+    <p class="text">Time Remaining Until {{ until }}</p>
+    <p class="time">{{ remaining }}</p>
   </div>
 </template>
 
 <style scoped>
+p {
+  margin: 0;
+  padding: 0;
+  line-height: 5rem;
+}
+
+.clock {
+  font-family: 'Share Tech Mono', monospace;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: #daf6ff;
+  text-shadow: 0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0);
+}
+
+.time {
+  letter-spacing: 0.05rem;
+  font-size: 5rem;
+}
+
+.text {
+  letter-spacing: 0.1rem;
+  font-size: 2rem;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
 import App from './App.vue'
+import './style.css'
+
+
+dayjs.extend(duration)
 
 createApp(App).mount('#app')
