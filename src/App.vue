a61dc793cec6d2d7b034d2c721cfa9a30b8d552f
kifuan <kifuan@foxmail.com>
2023-01-24 16:36:36 +0800

    feat: add link to source code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import until from './until.json'
 import Countdown from './components/Countdown.vue'
+import RepositoryLink from './components/SourceCodeLink.vue'
 </script>
 
 <template>
   <Countdown :until="until" />
+  <RepositoryLink href="https://github.com/kifuan/countdown" />
 </template>
 
 <style>
@@ -13,6 +15,9 @@ html, body {
 }
 
 body {
+  font-family: 'Share Tech Mono', monospace;
+  text-align: center;
+  text-shadow: 0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0);
   background: #0f3854;
   background: radial-gradient(ellipse at center,  #0a2e38  0%, #000000 100%);
   overflow: hidden;
