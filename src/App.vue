05faecc44dbb1a57be74282adf29b676d8363721
kifuan <kifuan@foxmail.com>
2023-01-24 17:44:20 +0800

    refactor: use downloaded font from google fonts

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,3 @@ import GitHubLink from './components/GitHubLink.vue'
   <Countdown :until="until" />
   <GitHubLink href="https://github.com/kifuan/countdown" />
 </template>
-
-<style>
-html, body {
-  height: 100%;
-}
-
-body {
-  font-family: 'Share Tech Mono', monospace;
-  text-align: center;
-  text-shadow: 0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0);
-  background: #0f3854;
-  background: radial-gradient(ellipse at center,  #0a2e38  0%, #000000 100%);
-  overflow: hidden;
-  padding: 0.5rem;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import App from './App.vue'
+import './style.css'
 
 dayjs.extend(duration)
 
