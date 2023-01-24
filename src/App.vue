8709184876914f2bf8e40e633eaf638a9854a0fd
kifuan <kifuan@foxmail.com>
2023-01-24 14:13:33 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useNow, useDateFormat } from '@vueuse/core'
+
+const now = useNow()
+const date = useDateFormat(now, 'YYYY-MM-DD')
+const time = useDateFormat(now, 'HH:mm:ss')
+</script>
+
+<template>
+  <div class="clock">
+    <p class="date">{{ date }}</p>
+    <p class="time">{{ time }}</p>
+    <p class="text">THIS IS TEXT</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
