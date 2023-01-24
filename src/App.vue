5e45a1699603750c964ea6657999c707ac91d3c1
kifuan <kifuan@foxmail.com>
2023-01-24 14:54:59 +0800

    refactor: move styles to style.css

--- src/App.vue
+++ src/App.vue
@@ -21,32 +21,3 @@ update()
     <p class="time">{{ remaining }}</p>
   </div>
 </template>
-
-<style scoped>
-p {
-  margin: 0;
-  padding: 0;
-  line-height: 5rem;
-}
-
-.clock {
-  font-family: 'Share Tech Mono', monospace;
-  text-align: center;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  color: #daf6ff;
-  text-shadow: 0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0);
-}
-
-.time {
-  letter-spacing: 0.05rem;
-  font-size: 5rem;
-}
-
-.text {
-  letter-spacing: 0.1rem;
-  font-size: 2rem;
-}
-</style>
